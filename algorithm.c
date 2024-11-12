b8a5dfaa3147c9cd0dd1201ff01e5a3f14c05552
Flávia Gonçalves <129198398+FlaviaMarcella@users.noreply.github.com>
2024-11-12 10:51:45 -0300

    upload main files

--- algorithm.c
+++ algorithm.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
-int main(){
-    printf("Hello, World!");
+#include <stdlib.h>
+ 
+void helloWorld(void){
+    printf("Hello World!\n");
 }
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "algorithm.h"
+
+int main(){
+	helloWorld();
+	return (0);
+}
